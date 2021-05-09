02315b4aa858c9061e7db35ec635bc49cea235c5
Lenovo <lordoftrident@gmail.com>
2021-05-09 14:52:40 +0200

    command line arguments

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,35 +1,54 @@
 #include "../include/COMPONENTS"
+#include "../include/INFO"
 
 #include "./Temple/Shell.hpp"
 
 int main(int argc, char* argv[]) {
+    std::vector <std::string> params;
+    bool skip = false;
     std::string File = "";
 
     // Reading parameters
-    bool skip = false;
     for (int i = 1; i < argc; ++ i) {
-        if (!skip) {
-            std::string arg = argv[i];
+        if (skip) {skip = false; continue;};
 
-            if (arg == "-f") {
+        std::string arg = argv[i];
 
-                skip = true;
-                if (i + 1 != argc) File = argv[i + 1];
-                else {
-                    std::cout << "Expected file name" << std::endl;
+        if (std::count(params.begin(),params.end(), arg)) {
+            std::cout << "Too many identical \"" + arg + "\" parameters." << std::endl; 
+            
+            return 1;         
+        };
 
-                    return 1;
-                };
+        if (arg == "-c" || arg == "-v") {params.push_back(arg); continue;};
+        if (arg == "-f") {
+            params.push_back(arg); 
+
+            if (argc - 1 > i) File = argv[i + 1];
+            else {std::cout << "Expected a file name." << std::endl; return 1;};
+
+            if (!LOT::plusplus::FileExists(File)) {
+                std::cout << "File \"" + File + "\" not found." << std::endl;
+                
+                return 1;
             };
-        } else skip = false;
+
+            skip = true;
+            continue;
+        };
+
+        std::cout << "Unknown parameter \"" + arg + "\"." << std::endl; 
+        
+        return 1;
     };
 
-    LOT::Temple::Temple TM_Compiler;
-
-    if (File == "") LOT::Temple::Shell::Start();
-    else if (LOT::plusplus::FileExists(File)) return TM_Compiler.Compile(LOT::plusplus::FileToString(File));
-    
-    std::cout << "File '" + File + "' not found." << std::endl;
+    if (std::count(params.begin(),params.end(), "-v")) std::cout << (std::string) "v" + VER << std::endl;
+    if (std::count(params.begin(),params.end(), "-c")) {
+        if (std::count(params.begin(),params.end(), "-f")) {
+            LOT::Temple::Temple TM_Compiler;
+            return TM_Compiler.Compile(LOT::plusplus::FileToString(File));
+        } else LOT::Temple::Shell::Start();
+    };
     
     return 0;
 };
